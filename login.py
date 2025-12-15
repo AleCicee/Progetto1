d2e14533959058a9471cdd8e15181f48f5354ed8
Firegà <leonardograziano72@gmail.com>
2025-12-15 08:36:46 +0100

    login.py : LogIn SignIn p1

--- login.py
+++ login.py
@@ -1,4 +1,14 @@
+UTENTI = {admin : 123}
 def login(username,password):
     print("Login function called")
-    
-    
+    if username in UTENTI and UTENTI[username] == password:
+        return True
+    else:
+        return False
+
+def signin(username,password):
+    if username in UTENTI:
+        return False
+    else:
+        UTENTI[username] = password
+        return True
